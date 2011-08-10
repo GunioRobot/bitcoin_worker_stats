f494eb86df2774e385c1bbad98f97ba7dad103cc
IceyEC <chmacnaughton@gmail.com>
2011-08-10 15:34:27 -0500

    removing config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-﻿<?php
-$api_key="7e468d0bda6a6c98cdf7e1173d65c4f315fb6f4937160049dff1af4775ab9db2";
-$host = "localhost";
-$dbuser = "bitcoin_stats";
-$dbpassword = "password";
-$database = "bitcoin_worker";
-?>
